4bca7821bfa390b982b1e538d4286c1181cfafea
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-03-25 16:02:47 +0100

    Завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+// Створюємо масив galleryItems
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+// Вставляємо після всіх дітей
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview,original,description }) => {
+        return `<div class='gallery__item'> 
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  </div>`;
+    })
+    .join('');
+};
+
+const lightbox = newSimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
 console.log(galleryItems);
